3d9036b38b7c6586e0242a729700390b866dfeb0
Palanikannan M <akashmalinimurugu@gmail.com>
2026-03-27 16:35:44 +0530

    fix: remove dead code

--- integrations/zellij/src/index.ts
+++
@@ -1,8 +0,0 @@
-import type { PluginAPI } from "@opensessions/core";
-import { ZellijProvider } from "./provider";
-
-export default function (api: PluginAPI): void {
-  api.registerMux(new ZellijProvider());
-}
-
-export { ZellijProvider } from "./provider";
